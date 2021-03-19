9e42a92688887d482a57e3a2c8936b8d526acf93
Charlotte Hetzler <charlotte@hetzler.de>
2021-03-19 22:32:28 +0200

    add create + update chatRoom endpoints

--- src/resolvers/chatRoom.ts
+++ src/resolvers/chatRoom.ts
@@ -3,6 +3,7 @@ import { Message } from '../entity/message';
 import { ChatRoom } from '../entity/chatRoom';
 import { GigUser } from '../entity/gigUser';
 import { ChatRoomUser } from '../entity/chatRoomUser';
+import { MessageResolver, MessageInput } from './message';
 
 @InputType()
 export class ChatRoomQuery {
@@ -14,10 +15,17 @@ export class ChatRoomQuery {
 export class ChatRoomInput {
     @Field({ nullable: false })
     lastMessageId: number;
+}
+
+@InputType()
+export class NewChatInput {
+    @Field({ nullable: false })
+    currentUserId: number;
 
     @Field({ nullable: false })
-    chatRoomId: number;
+    userId: number;
 }
+
 
 @Resolver(of => ChatRoom)
 export class ChatRoomResolver {
@@ -45,25 +53,85 @@ export class ChatRoomResolver {
         }
     }
 
-    @Mutation(() => ChatRoom)
-    async updateChatRoomLastMessage(@Arg('input', () => ChatRoomInput) input: ChatRoomInput) : Promise <ChatRoom>{
+    @Query(returns => ChatRoom)
+    async getCommonChatRoom (
+        @Arg('currentUserId') currentUserId: number,
+        @Arg('userId') userId: number
+    ) : Promise <ChatRoom> {
         try {
-            const chatRoom = await ChatRoom.findOne(input.chatRoomId);
-            chatRoom.lastMessageId = input.lastMessageId;
-            await chatRoom.save();
-            return chatRoom;
+            const currentUser = await GigUser.findOne(currentUserId);
+            const user = await GigUser.findOne(userId);
+
+            const currentUserChatRooms = await ChatRoomUser.find({where: {user: currentUser}, relations: ['user', 'chatRoom'] });
+            const userChatRooms = await ChatRoomUser.find({where: {user: user}, relations: ['user', 'chatRoom'] });
+
+            if (currentUserChatRooms.length > 0 && userChatRooms.length > 0 ) {
+                return await this.getCommonChat(currentUserChatRooms, userChatRooms);
+            }
+            
         } catch (error) {
-            throw `ChatRoomResolver.updateChatRoomLastMessage errored for chatRoom: ${input.chatRoomId}, lastMessageId: ${input.lastMessageId}. Error Msg: ${error}`;
+            throw `ChatRoomResolver.getCommonChatRoom errored. Error Msg: ${error}`;            
         }
     }
 
     @FieldResolver(() => Message)
-    async lastMessage(@Root() chatRoom: ChatRoom) : Promise <Message> {
+    async lastMessage(@Root() chatRoom: ChatRoom): Promise <Message> {
         return await Message.findOne({where: {id: chatRoom.lastMessageId}, relations: ['user']});
     };
 
     @FieldResolver(() => [ChatRoomUser])
-    async members(@Root() chatRoom: ChatRoom) : Promise <ChatRoomUser[]> {
+    async members(@Root() chatRoom: ChatRoom): Promise <ChatRoomUser[]> {
         return await ChatRoomUser.find({where: {chatRoom: chatRoom}, relations:['user']});
     };
+
+    @Mutation(() => ChatRoom)
+    async updateChatRoomLastMessage(@Arg('input', () => ChatRoomInput) input: ChatRoomInput) : Promise <ChatRoom>{
+        try {
+            const lastMessage = await Message.findOne({where: {id: input.lastMessageId}, relations: ['chatRoom']});
+            const chatRoom = await ChatRoom.findOne(lastMessage.chatRoom.id);
+            
+            chatRoom.lastMessageId = input.lastMessageId;
+            await chatRoom.save();
+            return chatRoom;
+        } catch (error) {
+            throw `ChatRoomResolver.updateChatRoomLastMessage errored for lastMessageId: ${input.lastMessageId}. Error Msg: ${error}`;
+        }
+    }
+
+    @Mutation(() => ChatRoom)
+    async createChatRoom(@Arg('input', () => NewChatInput) input: NewChatInput) : Promise <ChatRoom>{
+        try {
+            const chatRoom = new ChatRoom();
+            await chatRoom.save();
+            
+            const currentUser = await GigUser.findOne(input.currentUserId);
+            const user = await GigUser.findOne(input.userId);
+
+            await this.createChatRoomUser(currentUser, chatRoom);
+            await this.createChatRoomUser(user, chatRoom);
+            
+            return chatRoom
+        } catch (error) {
+            throw `ChatRoomResolver.createChatRoom errored for currentUserId: ${input.currentUserId}, userId: ${input.userId}. Error Msg: ${error}`;
+        }
+    }
+
+    async getCommonChat(currentUserChatRooms: ChatRoomUser[], userChatRooms: ChatRoomUser[]) {
+        for (const currentUserChatRoom of currentUserChatRooms) {
+            console.log(currentUserChatRoom)
+            for (const userChatRoom of userChatRooms) {
+                if (currentUserChatRoom.chatRoom.id === userChatRoom.chatRoom.id) {
+                    return await ChatRoom.findOne(userChatRoom.chatRoom.id);
+                }
+            }
+        }
+    }
+
+    async createChatRoomUser(user: GigUser, chatRoom: ChatRoom) : Promise <ChatRoomUser> {
+        const chatRoomUser = new ChatRoomUser();
+        chatRoomUser.user = user;
+        chatRoomUser.chatRoom = chatRoom;
+        await chatRoomUser.save();
+        return chatRoomUser;
+    }
 }
